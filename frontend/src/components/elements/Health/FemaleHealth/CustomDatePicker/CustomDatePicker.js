ac2359be5cf8bb2147dc2a46d7894e9870501c0b
GorgiY1 <orgeorgij@gmail.com>
2025-09-17 19:25:25 +0300

    Add components: DatePicker, Select

--- frontend/src/components/elements/Health/FemaleHealth/CustomDatePicker/CustomDatePicker.js
+++ frontend/src/components/elements/Health/FemaleHealth/CustomDatePicker/CustomDatePicker.js
@@ -43,14 +43,10 @@ const YearMonthSelect = ({ value, options, onChange, className = '' }) => {
       <div className="select-header" onClick={toggleDropdown}>
         <div className="select-input">
           {currentOption?.label || '—'}
-          
-          <svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M22 2L12 12L2 2" stroke="#116FCF" stroke-width="3" stroke-linecap="round" />
-</svg>
-{/* <span className="date-select-arrow">
-        </span> */}
+          <svg className="date-select-arrow" width="20" height="11" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M22 2L12 12L2 2" stroke="#116FCF" stroke-width="3" stroke-linecap="round" />
+          </svg>
         </div>
-        
       </div>
       
       {isOpen && (
@@ -175,15 +171,20 @@ const CustomBirthdateDatePicker = ({ selected, onChange, placeholder, className 
     >
       {selected ? (
         <div className="date-with-dots">
-          <span className="date-part">{selected.getDate().toString().padStart(2, '0')}</span>
-          <span className="dot-separator"> ● </span>
-          <span className="date-part">{(selected.getMonth() + 1).toString().padStart(2, '0')}</span>
-          <span className="dot-separator"> ● </span>
-          <span className="date-part">{selected.getFullYear()}</span>
+            <span className="date-part">{selected.getDate().toString().padStart(2, '0')}</span>
+            <span className="dot-separator"> ● </span>
+            <span className="date-part">{(selected.getMonth() + 1).toString().padStart(2, '0')}</span>
+            <span className="dot-separator"> ● </span>
+            <span className="date-part">{selected.getFullYear()}</span>
+            
+              <svg className='date-with-dots-clndr-icon' width="15" height="17" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.7778 19.8H2.22222V7.7H17.7778M14.4444 0V2.2H5.55556V0H3.33333V2.2H2.22222C0.988889 2.2 0 3.179 0 4.4V19.8C0 20.3835 0.234126 20.9431 0.650874 21.3556C1.06762 21.7682 1.63285 22 2.22222 22H17.7778C18.3671 22 18.9324 21.7682 19.3491 21.3556C19.7659 20.9431 20 20.3835 20 19.8V4.4C20 3.81652 19.7659 3.25695 19.3491 2.84437C18.9324 2.43179 18.3671 2.2 17.7778 2.2H16.6667V0M15.5556 12.1H10V17.6H15.5556V12.1Z" fill="white" />
+              </svg>  
+     
         </div>
       ) : (
         <span className="female-data-picker-placeholder">{placeholder}
-        <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="15" height="17" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M17.7778 19.8H2.22222V7.7H17.7778M14.4444 0V2.2H5.55556V0H3.33333V2.2H2.22222C0.988889 2.2 0 3.179 0 4.4V19.8C0 20.3835 0.234126 20.9431 0.650874 21.3556C1.06762 21.7682 1.63285 22 2.22222 22H17.7778C18.3671 22 18.9324 21.7682 19.3491 21.3556C19.7659 20.9431 20 20.3835 20 19.8V4.4C20 3.81652 19.7659 3.25695 19.3491 2.84437C18.9324 2.43179 18.3671 2.2 17.7778 2.2H16.6667V0M15.5556 12.1H10V17.6H15.5556V12.1Z" fill="white" />
 </svg>
         {/* <img src={calendar_icon} alt="" className="female-calendar-icon" /> */}
@@ -192,31 +193,53 @@ const CustomBirthdateDatePicker = ({ selected, onChange, placeholder, className 
     </div>
   ));
 const CustomStyleDatePickerWrapper = styled.div`
+.react-datepicker-popper {
+  transform: translate(0px, 59px) !important;
+}
+.year-month-selectors {
+    margin: 10px 0 0;
+}
 .year-selector .select-input {
-    font-size: 24px;
-}
-.react-datepicker {
-  width: 412px;
-  height: 305px;
-}
-
-.react-datepicker__day {
   font-size: 20px;
 }
-
-.react-datepicker__day {
-  height: 30px; 
-  width: 30px; 
+/* Стрілка селектора */
+.date-select-arrow {
+  margin-left: 10px;
+  margin-top: 5px;
+  transform: rotate(0deg);
 }
-  .month-selector .select-input {
-    font-size: 20px;
+.react-datepicker {
+  width: 340px;
+  height: 340px;
+}
+.react-datepicker__day {
+  height: 25px; 
+  width: 25px;
+  font-size: 16px;
+}
+.react-datepicker__day-names {
+  margin-left: 25px;
+}
+.react-datepicker__day-name
+{
+  font-size: 16px;
+}
+.react-datepicker__week {
+  height: 35px;
+}
+.month-selector .select-input {
+  font-size: 18px;
+}
+.year-month-select .select-option {
+  font-size: 16px;
+  font-weight: 400;
 }
 `;
 
   return (
     <div className={`custom-datepicker-container ${hasValue(selected) ? 'has-value' : ''} ${className}`}>
-        <CustomStyleDatePickerWrapper>
-      <DatePicker
+    <CustomStyleDatePickerWrapper>
+      <DatePicker popperContainer={({ children }) => <div className="datepicker-portal">{children}</div>}
         selected={selected}
         onChange={handleDayClick}
         customInput={<CustomInput />}
@@ -252,6 +275,7 @@ const CustomStyleDatePickerWrapper = styled.div`
             ? 'selected-day'
             : 'normal-day'
         }
+        popperClassName="react-datepicker-popper"
       /></CustomStyleDatePickerWrapper>
     </div>
   );
